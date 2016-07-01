ff01c4532893478657a87716e312d3a5a2e8dbf4
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro.local>
2016-07-01 12:48:14 -0700

    Spent a bunch of time thinking about the end results yesterday and playing with the Instagram API. Today started over, got form and results pages working. Set up a hashtag on Instagram to test with. Moving on to calling the Instagram endpoint and rendering results

---
+++ server.py
@@ -0,0 +1,58 @@
+"""Instachallenge - Write an app that takes a hashtag, start date, and end date, 
+and collects submissions from Instagram """
+# Backend: Provide an API that accepts a POST to create a collection and GET to retrieve content 
+
+# from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+# from model import connect_to_db, db
+# User, List, Location, Category, Item, copy_items_to_db
+
+app = Flask(__name__)
+
+# For debug toolbar
+app.secret_key = "ABC"
+
+# Defining variable for Jinja2 to avoid it failing silently (this raises and error)
+# app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """landing page"""
+    
+    return render_template ("form.html")
+
+@app.route('/results', methods=["POST"])
+def show_results():
+
+    hashtag = request.form.get("hashtag")
+    startDate = request.form.get("startDate")
+    endDate = request.form.get("endDate")
+
+#why do I have to use 3 separate flashes?
+
+    flash("Hi You chose hashtag %s" % hashtag)
+    flash("Hi You chose start date %s" % startDate) 
+    flash("Hi You chose end date %s" % endDate)
+
+    return render_template ("results.html")
+# Endpoint: https://api.instagram.com/v1/tags/poorhoffermobile/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d
+
+ 
+if __name__ == "__main__":
+
+    app.debug = True
+
+    # connect_to_db(app)
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run()
+
+
+
+
+
