5579118d849b6f42d3b9e878c637b648329c6dae
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro.local>
2016-07-02 13:06:47 -0700

    set up the form to take in a hashtag, then on the server side take in that hashtag, concatenate it, and redirect to the API URL as the result. Next going a bit backwards and setting up db.

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 # from model import connect_to_db, db
 # User, List, Location, Category, Item, copy_items_to_db
 
+# from instagram import client, subscriptions
+
 app = Flask(__name__)
 
 # For debug toolbar
@@ -21,23 +23,22 @@ app.secret_key = "ABC"
 @app.route('/')
 def index():
     """landing page"""
-    
     return render_template ("form.html")
+
+    # construct a string - concatenate the info that comes in
+    # what's the python magic to call the URL and get the data back
 
 @app.route('/results', methods=["POST"])
 def show_results():
 
     hashtag = request.form.get("hashtag")
-    startDate = request.form.get("startDate")
-    endDate = request.form.get("endDate")
+    # startDate = request.form.get("startDate")
+    # endDate = request.form.get("endDate")
 
-#why do I have to use 3 separate flashes?
+    endpoint = "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d"
 
-    flash("Hi You chose hashtag %s" % hashtag)
-    flash("Hi You chose start date %s" % startDate) 
-    flash("Hi You chose end date %s" % endDate)
-
-    return render_template ("results.html")
+    return redirect (endpoint)
+    # return render_template ("results.html", hashtag=hashtag, endpoint=endpoint)
 # Endpoint: https://api.instagram.com/v1/tags/poorhoffermobile/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d
 
  
