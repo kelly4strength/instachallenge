c57d0f8a72887913422a88f92a8ead4284c4b679
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro.local>
2016-07-03 14:56:52 -0700

    working on getting the date from the endpoint into a dict so I can get the images out

--- model.py
+++ model.py
@@ -1,11 +1,46 @@
 # """Model and database functions for Pixlee Instachallenge project"""
 
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+
+# # This is the connection to the PostgreSQL database through
+# # the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# # object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
 
 # ##############################################################################
 # # Model definitions
+
+class Search(db.Model):
+    """search data for instachallenge"""
+
+    __tablename__ = "searches"
+
+    search_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tag = db.Column(db.String(164), nullable=True)
+    start_date = db.Column(db.String(80), nullable=True)
+    end_date = db.Column(db.String(64), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Search search_id=%s tag=%s start_date=%s end_date=%s>" % (self.search_id, self.tag, self.start_date, self.end_date)
+
+
+# def copy_items_to_db(categories, list_id, item_name, item_address, item_comments):
+
+#     for num in range(len(categories)):
+
+#         final_item = Item(list_id=list_id,
+#                     category_id=categories[num],
+#                     item_name=item_name[num],
+#                     item_address=item_address[num],
+#                     item_comments=item_comments[num])
+
+#         db.session.add(final_item)
+#         db.session.commit()
+
 
 
 
@@ -14,15 +49,32 @@
 # def connect_to_db(app, db_uri="postgresql:///instadb"):
 #     """Connect the database to our Flask app."""
 
-#     # Configure to use our PostgreSQL database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-#     db.app = app
-#     db.init_app(app)
+    # Configure to use our PostgreSQL database
+    # app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    # db.app = app
+    # db.init_app(app)
 
 # if __name__ == "__main__":
 #     from server import app
     
-#     connect_to_db(app)
-#     print "Connected to DB."
+    # connect_to_db(app)
+    # print "Connected to DB."
 
-#     db.create_all()
+    # db.create_all()
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///instas'
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    from server import app
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+    
+    connect_to_db(app)
+    print "Connected to DB."
--- server.py
+++ server.py
@@ -7,8 +7,7 @@ and collects submissions from Instagram """
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-# from model import connect_to_db, db
-# User, List, Location, Category, Item, copy_items_to_db
+from model import connect_to_db, db, Search
 
 # from instagram import client, subscriptions
 
@@ -22,34 +21,59 @@ app.secret_key = "ABC"
 
 @app.route('/')
 def index():
-    """landing page"""
+    """landing page where user can input tag for search"""
     return render_template ("form.html")
 
-    # construct a string - concatenate the info that comes in
-    # what's the python magic to call the URL and get the data back
 
 @app.route('/results', methods=["POST"])
 def show_results():
+    """shows the results from the instagram endpoint url made with the inputted tag"""
 
-    hashtag = request.form.get("hashtag")
-    # startDate = request.form.get("startDate")
-    # endDate = request.form.get("endDate")
+    tag = request.form.get("tag")
+    start_date = request.form.get("start_date")
+    end_date = request.form.get("end_date")
 
-    endpoint = "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d"
+    new_search = Search(tag=tag,
+                    start_date=start_date, 
+                    end_date=end_date)
+    db.session.add(new_search)
+    db.session.commit()
+
+    # construct a string - concatenate the info that comes in
+    endpoint = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d"
+        # what's the python magic to call the URL and get the data back
+
+    #how do I say get this data at the endpoint? 
+    # and then take data from the end point which is a string and make it into a dict
+    #then render the images found in the dict on the page?
+
+
+    # "data": [{"attribution": null, 
+    #         "tags": ["poorhoffermobile"], 
+    #         "type": "image", 
+    #         "location": null, 
+    #         "comments": {"count": 0}, 
+    #         "filter": "Normal", 
+    #         "created_time": "1467345183", 
+    #         "link": "https://www.instagram.com/p/BHTfEZtj9MF/", 
+    #         "likes": {"count": 0}, 
+
+    #         "images": {"low_resolution": {"url": "https://scontent.cdninstagram.com/
+    #         t51.2885-15/s320x320/e35/11420918_132887830470925_116640902_n.jpg?
+    #         ig_cache_key=MTI4NDUwNjk2MDY1ODQyMDQ4NQ%3D%3D.2", "width": 320, "height": 320}, 
+
 
     return redirect (endpoint)
     # return render_template ("results.html", hashtag=hashtag, endpoint=endpoint)
-# Endpoint: https://api.instagram.com/v1/tags/poorhoffermobile/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d
-
  
 if __name__ == "__main__":
 
     app.debug = True
 
-    # connect_to_db(app)
+    connect_to_db(app)
 
     # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    DebugToolbarExtension(app)
 
     app.run()
 
