c227527c632e2aa13e91faf960204dedf2b87e23
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro.local>
2016-07-09 17:34:03 -0700

    try and except added and working where the hashtag is not found, but need to add one for invalid dates, tested for the year 9999 and got an empty result but still a result - should go to the form page with invalid request error. Also still need to add pagination, I think that is killing the response for large requests.

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 """Instachallenge - Write an app that takes a hashtag, start date, and end date, 
 and collects submissions from Instagram """
-# Backend: Provide an API that accepts a POST to create a collection and GET to retrieve content 
 
 from jinja2 import StrictUndefined
 
@@ -10,7 +9,6 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db, Search
 # Result
- 
 # from helper import get_endpoint_data
 
 import requests
@@ -30,9 +28,6 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """landing page where user can input tag for search"""
-
-    # session.clear()
-    # print session
     
     return render_template ("form.html")
 
@@ -40,6 +35,8 @@ def index():
 @app.route('/results', methods=["POST"])
 def show_results():
     """shows the results from the instagram endpoint url made with the inputted tag"""
+
+    #TODO Add a try/except for 404 responses and render message if 404(not found)
 
     # Take in start and end dates from users and turn them into datetime > happens in the db
     tag = request.form.get("tag")
@@ -53,52 +50,78 @@ def show_results():
     db.session.add(new_search)
     db.session.commit()
 
+    # setting the search id in the session so that the urls can be updated 
     search_id = session['current_search'] = new_search.search_id
-    print session
-    print search_id
+    # print session
+    # print search_id
 
     # concatenate the endpoint url and given hashtag to construct the url
     endpoint = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d"
         
     # use the python requests library to call the endpoint URL and get the data (type(r)='requests.models.Response')
-    r = requests.get(endpoint)
+    r = requests.get(endpoint) 
+    # == None:
+    #         flash('No results found Please try another hashtag or change your date parameters. Thanks :)')
+    #         return render_template("form.html")
         
-    # Confirming response is good (200)
-    print r.status_code
-        
-    # set the response to json (dict)
-    huge_data = r.json()
+    if r.status_code != 200:
+        flash('No results found Please try another hashtag or change your date parameters. Thanks :)')
+        return render_template("form.html")
 
-    # For loop that loops over the huge_data[data] dictionary 
-    # and gets urls for each image in the response list and puts them in urls[]
-    urls = []
-    for i in huge_data['data']:
-        image_url = i['images']['thumbnail']['url']
-        # print image_url
-        caption_time = i['caption']['created_time']
-        # print caption_time
+    else:
+        # Confirming response is good (200)
+        print r.status_code
+            
+        # set the response to json (dict)
+        huge_data = r.json()
 
-        caption_datetime = (datetime.datetime.fromtimestamp(int(caption_time)).strftime('%Y-%m-%d %H:%M:%S'))
-        # print "caption_datetime"
-        # print caption_datetime
-        # print type(caption_datetime)
+        # print huge_data['pagination']
+        # print huge_data['pagination']['max_tag_id']
 
-        caption_datetime_too = (datetime.datetime.strptime(caption_datetime,'%Y-%m-%d %H:%M:%S'))
-        # print caption_datetime_too
-        # print type(caption_datetime_too)
+        # For loop that loops over the huge_data[data] dictionary 
+        # and gets urls for each image in the response list and puts them in urls[]
+        urls = []
 
-        # if caption_time is between start_date and end_date, add image urls to urls list
-        if new_search.start_date <= caption_datetime_too <= new_search.end_date:
-            urls.append(image_url)
-            print "urls"
-            print urls
+        try:
 
-    update = Search.query.filter_by(search_id=search_id).first()
-    update.urls=urls
-    db.session.commit()
+            for i in huge_data['data']:
+                image_url = i['images']['thumbnail']['url']
+                # print image_url
+                caption_time = i['caption']['created_time']
+                # print caption_time
 
-    # session.clear()
-    # print session
+                caption_datetime = (datetime.datetime.fromtimestamp(int(caption_time)).strftime('%Y-%m-%d %H:%M:%S'))
+                # print "caption_datetime"
+                # print caption_datetime
+                # print type(caption_datetime)
+
+                caption_datetime_too = (datetime.datetime.strptime(caption_datetime,'%Y-%m-%d %H:%M:%S'))
+                # print caption_datetime_too
+                # print type(caption_datetime_too)
+
+                # if caption_time is between start_date and end_date, add image urls to urls list
+                if new_search.start_date <= caption_datetime_too <= new_search.end_date:
+                    urls.append(image_url)
+                    print "urls"
+                    print urls
+
+            #implemented this as an update rather than another table
+            #TODO look at DB, see if additional table for storing more resulting data would simplify searches
+            update = Search.query.filter_by(search_id=search_id).first()
+            update.urls=urls
+            db.session.commit()
+
+        # This is working for calls where there are no urls for a hashtag
+            # but not when the dates are invalid 
+        except huge_data['data']['images']['thumbnail']['url'] == None:
+
+            # TODO figure out what and how to commit nothing if there are no URLs found
+            # update = Search.query.filter_by(search_id=search_id).first()
+            # update.urls=["no results found"]
+            # db.session.commit()
+            flash('No results found. Please try another hashtag or change your date parameters. Thanks :)')
+            
+            return render_template("form.html")
 
     return render_template ("results.html", tag=tag, urls=urls)
  
@@ -112,8 +135,6 @@ if __name__ == "__main__":
     DebugToolbarExtension(app)
 
     app.run()
-
-     # urls = get_endpoint_data(tag)
 
     # "data": 
         # user": {"username": "kelly4strength", 
